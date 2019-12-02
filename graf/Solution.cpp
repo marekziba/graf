d4943c6dd0b90f17ee57b498c258c264ed847120
Marek Zięba <marek.zieba@icloud.com>
2019-12-02 17:50:34 +0100

    Added constructor

--- graf/Solution.cpp
+++ graf/Solution.cpp
@@ -1,5 +1,15 @@
 #include "Solution.h"
+#include <random>
+#include <ctime>
 
 Solution::Solution(int nverts, int ncolors) {
+	_colors.resize(nverts);
+	std::srand(std::time(nullptr));
+	for (int i = 0; i < _colors.size(); i++) {
+		_colors[i] = std::rand() % ncolors;
+	}
+}
 
+int Solution::getFitness(Graf g) {
+	// Insert code here and git push
 }
